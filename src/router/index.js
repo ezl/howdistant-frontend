c13502c8ca54021ee508088b696f465e1213dae7
stan <shchoi22@gmail.com>
2020-09-12 08:11:11 +0900

    scroll top

--- src/router/index.js
+++ src/router/index.js
@@ -74,6 +74,12 @@ const routes = [
 const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    }
+    return { x: 0, y: 0 };
+  },
   routes
 });
 
